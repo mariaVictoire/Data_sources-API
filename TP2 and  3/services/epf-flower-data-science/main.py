98dfef848863233151fd6488f4397b8748f31091
mariaVictoire <159890985+mariaVictoire@users.noreply.github.com>
2024-11-26 11:49:48 +0100

    Ajout de la redirection de / vers /docs

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -1,8 +1,13 @@
 import uvicorn
-
+from fastapi.responses import RedirectResponse  # Import pour la redirection
 from src.app import get_application
 
 app = get_application()
+
+# Redirige la racine (/) vers la documentation Swagger (/docs)
+@app.get("/")
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
