26b8cd54955e658a0fcce80088c15ea9fff996b7
mariaVictoire <159890985+mariaVictoire@users.noreply.github.com>
2024-12-02 20:06:39 +0100

    organisation data et .gitignore

--- TP2 and  3/services/epf-flower-data-science/tests/unit/api/routes/test_hello.py
+++ TP2 and  3/services/epf-flower-data-science/tests/unit/api/routes/test_hello.py
@@ -13,7 +13,7 @@ class TestHelloRoute:
 
         app = get_application()
 
-        client = TestClient(app, base_url="http://testserver")
+        client = TestClient(app, base_url="http://127.0.0.1:8080")
 
         return client
 
