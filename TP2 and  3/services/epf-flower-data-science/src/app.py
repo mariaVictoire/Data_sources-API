8f75a5cc5286298149c2517a08718b1512654434
mariaVictoire <159890985+mariaVictoire@users.noreply.github.com>
2024-12-23 17:59:46 +0100

    endpoint de recupération, modification des paramètre depuis firestore

--- TP2 and  3/services/epf-flower-data-science/src/app.py
+++ TP2 and  3/services/epf-flower-data-science/src/app.py
@@ -2,6 +2,38 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from src.api.router import router 
 from src.api.routes.data import router as data_router
+from src.api.routes.parameters import router as parameters_router
+from src.api.routes.authentication import router as authentification_router
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.openapi.utils import get_openapi
+swagger_token = None
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="authentification/login")
+
+def custom_openapi():
+    """Personnalisation du schéma OpenAPI."""
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="epf-flower-data-science",
+        version="1.0.0", 
+        description="Fast API avec JWT Bearer Token",
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",  
+        }
+    }
+    # Appliquer BearerAuth sur toutes les routes
+    for path in openapi_schema["paths"].values():
+        for method in path.values():
+            method["security"] = [{"BearerAuth": []}]
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+ 
 
 
 def get_application() -> FastAPI:
@@ -22,6 +54,10 @@ def get_application() -> FastAPI:
 
     application.include_router(router)
     application.include_router(data_router, prefix="/data", tags=["data"])
+    application.include_router(parameters_router, prefix="/parameters", tags=["parameters"])
+    application.include_router(authentification_router, prefix="/authentification", tags=["authentification"])
+    application.openapi = custom_openapi
 
     return application
 
+app = get_application()
